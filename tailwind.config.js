570599de5634ff2be4912b8ef47ed7198109f597
yeonsy <108213868+yeonsy@users.noreply.github.com>
2023-04-29 15:01:03 -0400

    style: retab

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+		},
+	},
+	plugins: [],
 }
